0e842f8c9c6ff16270379997f004618bc5c81259
Ryanlzventura <ryan-luiz12@hotmail.com>
2024-08-25 19:09:29 -0300

    localStorage commit

--- script.js
+++ script.js
@@ -14,4 +14,45 @@ function toggleMode() {
   }
  
 }
+function toggleMode() {
+  const html = document.documentElement
+  html.classList.toggle("light")
+
+  // Salva o tema escolhido no localStorage
+  if (html.classList.contains("light")) {
+    localStorage.setItem("theme", "light")
+  } else {
+    localStorage.setItem("theme", "dark")
+  }
+
+  // Pega a tag img
+  const img = document.querySelector("#profile img")
+
+  // Substituir a imagem
+  if (html.classList.contains("light")) {
+    // Se tiver light mode, adicionar a imagem light
+    img.setAttribute("src", "./assets/Avatar-light.png")
+  } else {
+    // Se tiver sem light mode, manter a imagem normal
+    img.setAttribute("src", "./assets/Avatar-dark.png")
+  }
+}
+
+// Verifica o tema ao carregar a página
+function checkTheme() {
+  const theme = localStorage.getItem("theme")
+  const html = document.documentElement
+  const img = document.querySelector("#profile img")
+
+  if (theme === "light") {
+    html.classList.add("light")
+    img.setAttribute("src", "./assets/Avatar-light.png")
+  } else {
+    html.classList.remove("light")
+    img.setAttribute("src", "./assets/Avatar-dark.png")
+  }
+}
+
+// Chama a função ao carregar a página
+checkTheme()
   
